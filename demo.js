2dce077f81c22ec2820c024b53c0a2f67b684662
Selvaganesh <selvaganesh@Selvaganeshs-MacBook-Air.local>
2026-04-07 14:23:41 +0530

    my first js code

---
+++ demo.js
@@ -0,0 +1,34 @@
+//Variable
+//1. var 2. const 3. let
+
+//var - function scope(declare, rediclare, reassign)
+var a = 10
+console.log(a);
+var a = 30; //redeclare
+console.log(a);
+
+var a;
+a = 50; //reassign
+console.log(a);
+
+//let - block scope(declare, reassign)
+
+let ab = 10;
+console.log(ab);
+
+let b = 20
+console.log(b);
+
+// let a = 30;
+// console.log(a);
+
+//const(declare)-blockscope
+
+const number = 1;
+console.log(number);
+
+//const number =2
+// const variable;
+// variable = "name";
+// console.log(variable);
+
